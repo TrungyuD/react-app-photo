7194ed0a1f7710be8bf9fdde7a50e2321d457025
TrungyuD <shawnkool10@gmail.com>
2021-01-15 00:06:09 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
-// import './App.scss';
-import Header from './components/Header';
-import NotFound from './components/NotFound';
+import React from 'react';
 import HomePage from './SHHomePage/SHHomePage';
-
-// Lazy load - Code splitting
-const Photo = React.lazy(() => import('./features/Photo'));
 
 function App() {
   return (
-    <div className="photo-app">
-      {/* <Suspense fallback={<div>Loading ...</div>}>
-        <BrowserRouter>
-          <Header />
-          <Switch>
-            <Redirect exact from="/" to="/photos" />
-            <Route path="/photos" component={Photo} />
-            <Route component={NotFound} />
-          </Switch>
-        </BrowserRouter>
-      </Suspense> */}
+    <div className="">
       <HomePage />
     </div>
   );
